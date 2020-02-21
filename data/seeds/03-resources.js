563303d1b0992669371652830766c315d295e26d
unknown <viola4lfe@gmail.com>
2020-02-21 12:35:09 -0500

    seeded the resources table, unable to seed data at this point

--- data/seeds/03-resources.js
+++ data/seeds/03-resources.js
@@ -3,9 +3,25 @@ exports.seed = function(knex) {
   return knex("resources").then(function() {
     // Inserts seed entries
     return knex("resources").insert([
-      { id: 1, colName: "rowValue1" },
-      { id: 2, colName: "rowValue2" },
-      { id: 3, colName: "rowValue3" }
+      {
+        name: "Computer",
+        description: "15 inch laptop",
+        notes: "In good condition"
+      },
+      {
+        name: "Leash",
+        description: "30 leashes of all sizes",
+        notes: "Wash after every use"
+      },
+      {
+        name: "Paint",
+        description: "10 cans of cyan paint"
+      },
+      {
+        name: "Concrete",
+        description: "20lbs of concrete mix",
+        notes: "may need more"
+      }
     ]);
   });
 };
