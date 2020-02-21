9f80dac0d4ba61e48c5fd32d98002e939eb77aa7
unknown <viola4lfe@gmail.com>
2020-02-21 12:44:21 -0500

    seeded the task table, still unable to seed data

--- data/seeds/02-task.js
+++ data/seeds/02-task.js
@@ -3,9 +3,57 @@ exports.seed = function(knex) {
   return knex("task").then(function() {
     // Inserts seed entries
     return knex("task").insert([
-      { id: 1, colName: "rowValue1" },
-      { id: 2, colName: "rowValue2" },
-      { id: 3, colName: "rowValue3" }
+      {
+        project_id: 1,
+        description: "bring tools",
+        notes: "stick to the budget",
+        completed: true
+      },
+      {
+        project_id: 1,
+        description: "pick up helpers",
+        notes: "call jon in advance",
+        completed: false
+      },
+      {
+        project_id: 2,
+        description: "buy paint brushes",
+        notes: "stick to the budget",
+        completed: true
+      },
+      {
+        project_id: 2,
+        description: "put down tarp",
+        notes: "don't spill on carpet",
+        completed: false
+      },
+      {
+        project_id: 2,
+        description: "Preview job site",
+        completed: true
+      },
+      {
+        project_id: 3,
+        description: "Practice music",
+        completed: true
+      },
+      {
+        project_id: 3,
+        description: "Get suit cleaned",
+        notes: "remove buttons",
+        completed: false
+      },
+      {
+        project_id: 4,
+        description: "wash dog",
+        notes: "buy treats",
+        completed: false
+      },
+      {
+        project_id: 4,
+        description: "Get shot record",
+        completed: true
+      }
     ]);
   });
 };
