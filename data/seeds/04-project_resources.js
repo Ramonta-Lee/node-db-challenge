e8cd1b956b7dc8a5760fd7518f18aaf3a5ba7664
unknown <viola4lfe@gmail.com>
2020-02-21 12:50:33 -0500

    seeded the project_resources table with seed data

--- data/seeds/04-project_resources.js
+++ data/seeds/04-project_resources.js
@@ -3,9 +3,13 @@ exports.seed = function(knex) {
   return knex("project_resources").then(function() {
     // Inserts seed entries
     return knex("project_resources").insert([
-      { id: 1, colName: "rowValue1" },
-      { id: 2, colName: "rowValue2" },
-      { id: 3, colName: "rowValue3" }
+      { project_id: 1, resources_id: 1 },
+      { project_id: 1, resources_id: 2 },
+      { project_id: 1, resources_id: 3 },
+      { project_id: 2, resources_id: 1 },
+      { project_id: 2, resources_id: 2 },
+      { project_id: 3, resources_id: 1 },
+      { project_id: 4, resources_id: 1 }
     ]);
   });
 };
